bd7151af05e1407cb23cae6a75100c25a99fc594
Roman <ryaroshencko@gmail.com>
2024-04-03 14:40:45 +0300

    Add unit tests for activity

---
+++ Units/TestActivityClient.h
@@ -0,0 +1,46 @@
+#ifndef TESTACTIVITYCLIENT_H
+#define TESTACTIVITYCLIENT_H
+
+#include "managers/connections_manager.h"
+#include "datastorage/index/actorindex.h"
+#include <sqlite3.h>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+class TestActorIndex {
+
+    ActorIndex *actorIndex;
+
+    TestActorIndex(){
+        ExtraChainNode node;
+        actorIndex = new ActorIndex(node);
+    }
+
+    ~TestActorIndex(){
+        delete actorIndex;
+    }
+
+    TestActorIndex(const TestActorIndex&) = delete;
+    TestActorIndex& operator=(const TestActorIndex&) = delete;
+
+
+
+public:
+    static TestActorIndex& getInstance() {
+        std::filesystem::path filePath = dbActPath;
+        if (fs::exists(filePath))
+            fs::remove(filePath);
+        static TestActorIndex instance;
+        return instance;
+    }
+
+
+    ActorIndex* getActorIndex() const {
+        return actorIndex;
+    }
+
+};
+
+
+#endif // TESTACTIVITYCLIENT_H
