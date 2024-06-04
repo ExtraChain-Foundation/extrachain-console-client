639cc2cb8436347f30b28d66a6cd7c9507fee065
Yevhenii Reshetnik <yevhenii.reshetnik@gmail.com>
2024-06-04 13:14:38 +0200

    create vpn keys for server

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+#ifdef Q_OS_WIN
+    #include <DbgHelp.h>
+    #include <Windows.h>
+#endif
+
 #include <QCommandLineParser>
 #include <QCoreApplication>
 #include <QDebug>
@@ -5,6 +10,9 @@
 #include <QElapsedTimer>
 #include <QLockFile>
 #include <QStandardPaths>
+
+#include <csignal>
+#include <execinfo.h>
 
 #include "console/console_manager.h"
 #include "datastorage/dfs/dfs_controller.h"
@@ -16,6 +24,92 @@
     #include "preconfig.h"
 #endif
 
+#ifdef Q_OS_WIN
+const char* strsignal(int sig) {
+    static const std::unordered_map<int, const char*> sig2NameMap = {
+        { SIGINT, "SIGINT" }, { SIGTERM, "SIGTERM" }, { SIGABRT, "SIGABRT" }, { SIGSEGV, "SIGSEGV" }
+    };
+
+    const auto iter = sig2NameMap.find(sig);
+    if (iter != sig2NameMap.end())
+        return iter->second;
+
+    return "Unknown";
+}
+#endif
+
+static void HandleSignal(int sig) {
+    try {
+        switch (sig) {
+#ifdef Q_OS_LINUX
+        case SIGQUIT:
+#endif
+        case SIGINT:
+        case SIGTERM: {
+            qCritical() << "Signal achieved: " << strsignal(sig);
+            QCoreApplication::quit();
+            break;
+        }
+        case SIGABRT:
+        case SIGSEGV: {
+            qCritical() << "Catch signal: " << strsignal(sig);
+#ifdef Q_OS_WIN
+            //TODO: need to test this logic on Windows
+            constexpr int maxFrames = 64;
+            void* stackTrace[maxFrames];
+            USHORT frames = CaptureStackBackTrace(0, maxFrames, stackTrace, nullptr);
+            SYMBOL_INFO* symbol = (SYMBOL_INFO*)malloc(sizeof(SYMBOL_INFO) + 256 * sizeof(char));
+            symbol->MaxNameLen = 255;
+            symbol->SizeOfStruct = sizeof(SYMBOL_INFO);
+
+            for (USHORT i = 0; i < frames; ++i) {
+                SymFromAddr(GetCurrentProcess(), (DWORD64)(stackTrace[i]), 0, symbol);
+                qInfo() << "#" << i << ": " << symbol->Name << " - 0x" << std::hex << stackTrace[i];
+            }
+            free(symbol);
+#elif defined(Q_OS_LINUX)
+            void* stackTrace[64];
+            int frameCount = backtrace(stackTrace, 64);
+            char** symbols = backtrace_symbols(stackTrace, frameCount);
+            if (symbols != nullptr) {
+                for (int i = 0; i < frameCount; ++i)
+                    qInfo() << "#" << i << ": " << symbols[i];
+                free(symbols);
+            }
+#endif
+
+            if (sig == SIGSEGV)
+                _exit(EXIT_FAILURE);
+            else
+                exit(EXIT_FAILURE);
+        }
+        default:
+            exit(EXIT_FAILURE);
+        }
+    } catch (const std::exception& ex) {
+        qCritical() << "SignalHandler exception: " << ex.what();
+        _exit(EXIT_FAILURE);
+    }
+}
+
+bool SetupSignals() {
+#ifdef Q_OS_WIN
+    constexpr const std::array<int, 4> arrSig = { { SIGINT, SIGTERM, SIGSEGV, SIGABRT } };
+#else
+    constexpr const std::array<int, 5> arrSig = { { SIGINT, SIGTERM, SIGSEGV, SIGABRT, SIGQUIT } };
+#endif
+
+    for (const auto& item : arrSig) {
+        if (signal(item, HandleSignal) == SIG_ERR) {
+            qCritical() << "Cannot handle a signal: " + QString::fromStdString(strsignal(item))
+                    + ", reason - " + QString::fromStdString(strerror(errno));
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main(int argc, char* argv[]) {
     QCoreApplication app(argc, argv);
 
@@ -23,6 +117,9 @@ int main(int argc, char* argv[]) {
     app.setOrganizationName("ExtraChain Foundation");
     app.setOrganizationDomain("https://extrachain.io/");
     app.setApplicationVersion(EXTRACHAIN_VERSION);
+
+    if (!SetupSignals())
+        exit(EXIT_FAILURE);
 
     registerMetaTypes();
 
@@ -43,9 +140,10 @@ int main(int argc, char* argv[]) {
     QCommandLineOption core("core", "First network creation");
     QCommandLineOption importOption("import", "Import from file", "import");
     QCommandLineOption netdebOption("network-debug", "Print all messages. Only for debug build");
-    QCommandLineOption dfsLimitOption({"l", "limit"}, "Set limit", "dfs-limit");
-    parser.addOptions({ debugOption, dirOption, emailOption, passOption, inputOption, core,
-                        clearDataOption, importOption, netdebOption, dfsLimitOption });
+    QCommandLineOption dfsLimitOption({ "l", "limit" }, "Set limit", "dfs-limit");
+    QCommandLineOption vpnServer("vpn-server", "Start VPN server");
+    parser.addOptions({ debugOption, dirOption, emailOption, passOption, inputOption, core, clearDataOption,
+                        importOption, netdebOption, dfsLimitOption, vpnServer });
     parser.process(app);
 
     // TODO: allow absolute dir
@@ -58,15 +156,15 @@ int main(int argc, char* argv[]) {
         Utils::wipeDataFiles();
     }
 
-//    QLockFile lockFile(".console.lock");
-//    if (!lockFile.tryLock(100)) {
-//        if (!QFile::exists(".console.lock")) {
-//            qDebug() << "[Console] Unable to write files. Check folder permissions";
-//            return -1;
-//        }
-//        qDebug() << "[Console] Already running in directory" << QDir::currentPath();
-//        return -1;
-//    }
+    //    QLockFile lockFile(".console.lock");
+    //    if (!lockFile.tryLock(100)) {
+    //        if (!QFile::exists(".console.lock")) {
+    //            qDebug() << "[Console] Unable to write files. Check folder permissions";
+    //            return -1;
+    //        }
+    //        qDebug() << "[Console] Already running in directory" << QDir::currentPath();
+    //        return -1;
+    //    }
 
     LogsManager::debugLogs = parser.isSet(debugOption);
 #ifdef QT_DEBUG
@@ -160,6 +258,9 @@ int main(int argc, char* argv[]) {
                 qInfo() << "Error: No profiles files";
             std::exit(-1);
         }
+    } else if (node.accountController()->count() > 0) {
+        if (parser.isSet(vpnServer))
+            node.createVPNKeys();
     }
 
     return app.exec();
